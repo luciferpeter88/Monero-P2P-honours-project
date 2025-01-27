886fbe87c41cb9c0148957c13fe81b394ca86efe
luciferpeter88 <kaszapnagyp@gmail.com>
2025-01-27 21:23:02 +0000

    .

--- app/routes/_index/components/Card.jsx
+++ app/routes/_index/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ src, title, description }) {
   return (
-    <div className="col-span-8 md:col-span-4 lg:col-span-2 mt-8">
+    <div className="col-span-8 md:col-span-4 lg:col-span-2 mt-6">
       <div className="bg-third flex flex-wrap gap-y-3  justify-center cursor-pointer shadow-[0_2px_6px_-1px_rgba(0,0,0,0.3)] rounded-xl w-full px-5 py-5">
         <img src={src} className="w-40 h-40" alt="" />
         <div className="ml-4 flex-1 items-center justify-center mt-2">
--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -7,6 +7,7 @@ import currency from "./components/picture/Currency.png";
 import banking from "./components/picture/Banking.png";
 import cashflow from "./components/picture/Cashflow.png";
 import Card from "./components/Card";
+import "./styles.css";
 
 export const meta = () => {
   return [
@@ -18,7 +19,7 @@ export const meta = () => {
 export default function Index() {
   return (
     <React.Fragment>
-      <div className="grid grid-cols-8 container mx-auto gap-x-5 px-10 sm:px-0 h-min-screen">
+      {/* <div className="grid grid-cols-8 container mx-auto gap-x-5 px-10 sm:px-0 h-min-screen">
         <div className="col-span-8">
           <Navbar />
         </div>
@@ -71,6 +72,68 @@ export default function Index() {
           description="Enjoy trading with minimal transaction costs."
         />
         <div className="col-span-8 mt-5">
+          <Footer />
+        </div>
+      </div> */}
+      <div className="grid-container container mx-auto px-5">
+        <div className="f">
+          <Navbar />
+        </div>
+        <div className="s flex flex-col justify-center mt-5">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-medium bg-gradient-to-r from-orange-50 to-secondary bg-clip-text text-transparent leading-tight uppercase">
+            Trade Monero
+          </h1>
+          {/* <span className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-medium uppercase leading-tight text-gray-100 mt-2">
+            Securely and Privately
+          </span> */}
+          <p className="text-sm md:text-base lg:text-lg xl:text-xl leading-tight mt-5 text-gray-50">
+            Monero (XMR) is a leading privacy-focused cryptocurrency that uses
+            advanced cryptography to ensure your transactions remain
+            confidential and secure.
+          </p>
+          <p className="text-sm md:text-base lg:text-lg xl:text-xl leading-tight mt-5 bg-gradient-to-r from-orange-50 to-secondary bg-clip-text text-transparent">
+            Start trading Monero today and experience true financial freedom.
+          </p>
+          <button className="mt-8 inline-flex items-center justify-center rounded-xl bg-secondary hover:bg-opacity-80 py-3 px-6 w-full md:w-52 font-dm text-base font-medium text-white shadow-xl shadow-orange-400/15 transition-transform duration-200 ease-in-out hover:scale-[0.98]">
+            Get Started
+          </button>
+        </div>
+        <div className="sm flex justify-center lg:justify-end mt-10 lg:mt-0">
+          <img
+            className="w-full h-full max-w-md lg:max-w-lg object-contain"
+            src={svg}
+            alt="Catalogue-pana"
+          />
+        </div>
+        <div className="c1">
+          <Card
+            src={protection}
+            title="Protected"
+            description="Ensuring privacy and security in every trade."
+          />
+        </div>
+        <div className="c2">
+          <Card
+            src={currency}
+            title="Traiding"
+            description="Connect with anyone, anywhere in the world."
+          />
+        </div>
+        <div className="c3">
+          <Card
+            src={banking}
+            title="Bank-Free"
+            description="Trade without banks or intermediaries."
+          />
+        </div>
+        <div className="c4">
+          <Card
+            src={cashflow}
+            title="Minimal Fees"
+            description="Enjoy trading with minimal transaction costs."
+          />
+        </div>
+        <div className="l pt-5">
           <Footer />
         </div>
       </div>
