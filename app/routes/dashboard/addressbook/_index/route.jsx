0adcc90c9e95dd83f89936150e0e096055328372
luciferpeter88 <kaszapnagyp@gmail.com>
2025-02-02 11:16:32 +0000

    .

---
+++ app/routes/dashboard/addressbook/_index/route.jsx
@@ -0,0 +1,3 @@
+export default function Index() {
+  return <h1>Address Book Route</h1>;
+}
--- app/routes/dashboard/components/SideBar.jsx
+++ app/routes/dashboard/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import moneroCard from "../components/picture/card-background2.svg";
 import { LuHouse } from "react-icons/lu";
 import { LuArrowUpFromLine } from "react-icons/lu";
@@ -10,6 +10,19 @@ import { TiMessages } from "react-icons/ti";
 import { IoMdSettings } from "react-icons/io";
 
 export default function SideBar() {
+  const { pathname } = useLocation();
+  const primary = "bg-primary border-l-4 border-l-secondary text-secondary";
+  const secondary = "bg-third";
+  const isDashboard = pathname === "/dashboard" ? primary : secondary;
+  const isSend = pathname === "/dashboard/send" ? primary : secondary;
+  const isReceive = pathname === "/dashboard/receive" ? primary : secondary;
+  const isAddressBook =
+    pathname === "/dashboard/addressbook" ? primary : secondary;
+  const isTransactions =
+    pathname === "/dashboard/transactions" ? primary : secondary;
+  const isMarket = pathname === "/dashboard/market" ? primary : secondary;
+  const isMessages = pathname === "/dashboard/messages" ? primary : secondary;
+  const isSettings = pathname === "/dashboard/settings" ? primary : secondary;
   return (
     <div className="dashboardSidebar flex flex-col mt-5 rounded-xl">
       {/* <div className="flex flex-col items-center bg-third pt-10">
@@ -22,26 +35,68 @@ export default function SideBar() {
             <h1>Profile Picture</h1>
           </div>
         </div>
-        <NavigateTo text="Account" icon={<LuHouse />} />
-        <NavigateTo text="Send" icon={<LuArrowUpFromLine />} />
-        <NavigateTo text="Receive" icon={<LuArrowDownToLine />} />
-        <NavigateTo text="Address Book" icon={<TbAddressBook />} />
-        <NavigateTo text="Transactions" icon={<MdOutlineHistory />} />
-        <NavigateTo text="Market" icon={<FaExchangeAlt />} />
-        <NavigateTo text="Messages" icon={<TiMessages />} />
-        <NavigateTo text="Settings" icon={<IoMdSettings />} />
+        <NavigateTo
+          text="Account"
+          icon={<LuHouse />}
+          NavigateTo="/dashboard"
+          style={isDashboard}
+        />
+        <NavigateTo
+          text="Send"
+          icon={<LuArrowUpFromLine />}
+          NavigateTo="/dashboard/send"
+          style={isSend}
+        />
+        <NavigateTo
+          text="Receive"
+          icon={<LuArrowDownToLine />}
+          NavigateTo="/dashboard/receive"
+          style={isReceive}
+        />
+        <NavigateTo
+          text="Address Book"
+          icon={<TbAddressBook />}
+          NavigateTo="/dashboard/addressbook"
+          style={isAddressBook}
+        />
+        <NavigateTo
+          text="Transactions"
+          icon={<MdOutlineHistory />}
+          NavigateTo="/dashboard/transactions"
+          style={isTransactions}
+        />
+        <NavigateTo
+          text="Market"
+          icon={<FaExchangeAlt />}
+          NavigateTo="/dashboard/market"
+          style={isMarket}
+        />
+        <NavigateTo
+          text="Messages"
+          icon={<TiMessages />}
+          NavigateTo="/dashboard/messages"
+          style={isMessages}
+        />
+        <NavigateTo
+          text="Settings"
+          icon={<IoMdSettings />}
+          NavigateTo="/dashboard/settings"
+          style={isSettings}
+        />
       </div>
     </div>
   );
 }
 
-function NavigateTo({ text, icon }) {
+function NavigateTo({ text, icon, NavigateTo, style }) {
   return (
     <Link
-      to="/"
+      to={NavigateTo}
       className="text-white font-medium text-sm flex items-center justify-center"
     >
-      <div className=" py-3 pl-2  border-primary hover:bg-primary hover:border-l-4 hover:border-l-secondary hover:text-secondary w-[90%] rounded-lg flex items-center gap-x-2">
+      <div
+        className={`py-3 pl-2 w-[90%] rounded-lg flex items-center gap-x-2 ${style}`}
+      >
         {icon}
         {text}
       </div>
